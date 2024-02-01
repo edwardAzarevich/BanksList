cc5b3f98e7588945f93a80327fcc611eb42fc89d
Edward Azarevich <edward.azarevich@gmail.com>
2024-02-02 01:31:04 +0300

    add tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Main from './views/Main'
 
 function App() {
-    const [count, setCount] = useState(0)
 
     return (
         <>
             <Main />
+            <div>
+                <h1 className='text-3x1'>Text with railwind css</h1>
+            </div>
+
         </>
     )
 }
